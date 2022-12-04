b0cbfe399ad6f30e0dbdc21d6422c5baef00d8c9
Simon Mårtensson <martensson.simon@gmail.com>
2022-12-04 12:11:03 +0100

    Add more unit tests for ResponseProcessor

--- tests/ResponseProcessorTests.cs
+++ tests/ResponseProcessorTests.cs
@@ -20,6 +20,16 @@ namespace PilotAppLib.Clients.MetNorway.Tests
             "ESSA AMD 040657Z 0406/0506 36003KT 9999 BKN025 PROB40 0406/0412 BKN005 PROB40 0418/0506 BKN004="
             )]
         [InlineData(
+            "ESGR",
+            "ESGR AUTO 031350Z 07008KT 9999 OVC023/// M01/M03 Q1030=",
+            "ESGR AUTO 031350Z 07008KT 9999 OVC023/// M01/M03 Q1030="
+            )]
+        [InlineData(
+            "ESGR",
+            "ESGR 031350Z 07008KT 9999 OVC023/// M01/M03 Q1030=",
+            "ESGR 031350Z 07008KT 9999 OVC023/// M01/M03 Q1030="
+            )]
+        [InlineData(
             "ESGJ",
             " METAR 1 \r\n METAR 2 \r\n\r\n AUTO ESGJ 031350Z 07008KT 9999 OVC023/// M01/M03 Q1030= \n ",
             "ESGJ AUTO 031350Z 07008KT 9999 OVC023/// M01/M03 Q1030="
